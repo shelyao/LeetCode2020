3874131a1fbab9ca3945ad002bc23630d82b44ae
shelyao <shelxy2010@gmail.com>
2020-11-18 18:41:01 -0500

    Nov 18 2020

--- 72-Edit Distance.py
+++ 72-Edit Distance.py
@@ -1,7 +1,7 @@
 from functools import lru_cache
 class Solution:
     # Top down
-    def minDistance(self, word1: str, word2: str) -> int:
+    def minDistance0(self, word1: str, word2: str) -> int:
         m, n = len(word1), len(word2)
         if m == 0 or n == 0: return m + n
         
@@ -19,12 +19,10 @@ class Solution:
                 else:
                     dp[i][j] = min(dp[i-1][j-1] + 1, dp[i-1][j]+1, dp[i][j-1]+1)
         
-        return dp[-1][-1]
-    
+        return dp[-1][-1]    
     # buttom up
-    def minDistance(self, word1: str, word2: str) -> int:
+    def minDistance1(self, word1: str, word2: str) -> int:
         m, n = len(word1), len(word2)
-
         @lru_cache(None)
         def helper(i, j):
             if i < 0 or j < 0: return (i+j+2)
@@ -33,4 +31,21 @@ class Solution:
             else:
                 return 1 + min(helper(i-1, j-1), helper(i-1,j), helper(i,j-1))
         
+        return helper(m-1, n-1) 
+    # memo
+    def minDistance(self, word1: str, word2: str) -> int:
+        m, n = len(word1), len(word2)
+        memo = {}
+        
+        def helper(i, j):
+            if i < 0 or j < 0: return (i+j+2)
+            if (i, j) not in memo:
+                if word1[i] == word2[j]:
+                    res = helper(i-1, j-1)
+                else:
+                    res = 1 + min(helper(i-1, j-1), helper(i-1,j), helper(i,j-1))
+                memo[(i, j)] = res
+            return memo[(i,j)]
+        
         return helper(m-1, n-1)
+        
